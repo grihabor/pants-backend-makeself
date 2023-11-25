d4d19f4f5a219e80c7c825b8138bbb5f0fd5e9da
Borodin Gregory <grihabor@gmail.com>
2023-11-25 23:34:53 +0100

    Rule graph works

--- pants-plugins/makeself/pants/goals/package.py
+++ pants-plugins/makeself/pants/goals/package.py
@@ -1,82 +1,69 @@
-import os
+import logging
 from dataclasses import dataclass
 
-from pants.core.util_rules.environments import EnvironmentName
-
-from makeself.pants.makeself import MakeselfBinary, MakeselfProcess
-from makeself.pants.target_types import (
-    MakeselfBinaryDependencies,
-    MakeselfBinaryOutputPath,
-    MakeselfBinaryStartupScript,
-)
 from pants.core.goals.package import BuiltPackage, BuiltPackageArtifact, PackageFieldSet
-from pants.core.goals.package import BuiltPackage, BuiltPackageArtifact, PackageFieldSet
-from pants.engine.addresses import Address, Addresses
-from pants.engine.process import ProcessResult
-from pants.engine.rules import Get, MultiGet, collect_rules, rule
-from pants.engine.target import DependenciesRequest
+from pants.core.util_rules.source_files import SourceFiles, SourceFilesRequest
+from pants.engine.env_vars import EnvironmentVars, EnvironmentVarsRequest
+from pants.engine.fs import CreateDigest, Digest, DigestContents, FileContent
+from pants.engine.rules import Get, collect_rules, rule
 from pants.engine.unions import UnionRule
-from pants.util.logging import LogLevel
+
+from makeself.pants.target_types import MakeselfBinaryStartupScript
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True)
-class BuiltMakeselfArtifact(BuiltPackageArtifact):
+class BuiltMakeselfBinaryArtifact(BuiltPackageArtifact):
     @classmethod
-    def create(cls, relpath: str) -> "BuiltMakeselfArtifact":
+    def create(cls, relpath: str) -> "BuiltMakeselfBinaryArtifact":
         return cls(
             relpath=relpath,
-            extra_log_lines=(f"Built Makeself chart artifact: {relpath}",),
+            extra_log_lines=(f"Built makeself binary: {relpath}",),
         )
 
 
 @dataclass(frozen=True)
-class MakeselfPackageFieldSet(PackageFieldSet):
+class MakeselfBinaryPackageFieldSet(PackageFieldSet):
     required_fields = (MakeselfBinaryStartupScript,)
 
     startup_script: MakeselfBinaryStartupScript
-    # dependencies: MakeselfBinaryDependencies
-    # output_path: MakeselfBinaryOutputPath
 
 
-@rule(desc="Package self-extractable archive", level=LogLevel.INFO)
+@rule
 async def package_makeself_binary(
-    field_set: MakeselfPackageFieldSet,
-    #tool: MakeselfBinary,
-    # _: EnvironmentName,
+    field_set: MakeselfBinaryPackageFieldSet,
 ) -> BuiltPackage:
-    result_dir = "__out"
-    # raise RuntimeError("")
+    """Substitute env vars in k8s source"""
+    source_files = await Get(
+        SourceFiles,
+        SourceFilesRequest([field_set.source]),
+    )
+    files = await Get(DigestContents, Digest, source_files.snapshot.digest)
+    assert len(files) == 1
+    file_content = files[0]
+    logger.debug("k8s source %s", file_content)
+    rendered_path = file_content.path + ".rendered"
 
-    startup_script, dependencies = await MultiGet(
-        Get(Address, field_set.startup_script),
-        Get(Addresses, DependenciesRequest(field_set.dependencies)),
+    # TODO use options_env_aware
+    env = await Get(
+        EnvironmentVars,
+        EnvironmentVarsRequest,
+        EnvironmentVarsRequest(requested=["VERSION"]),
+    )
+    logger.debug("env %s", env)
+    rendered_content = (
+        file_content.content.decode("utf-8").format(**env).encode("utf-8")
+    )
+    digest = await Get(
+        Digest, CreateDigest([FileContent(rendered_path, rendered_content)])
     )
 
-    process_output_file = os.path.join(
-        result_dir, f"{field_set.address.target_name}.run"
-    )
-
-    dependencies[0].target_name
-    await Get(
-        ProcessResult,
-        MakeselfProcess(
-            argv=[archive_dir, file_name, label, startup_script],
-            input_digest=result_digest,
-            output_files=(process_output_file,),
-            description=f"Packaging Makeself binary: {field_set.address}",
-        ),
-    )
-
-    return BuiltPackage(
-        final_snapshot.digest,
-        artifacts=tuple(
-            BuiltMakeselfArtifact.create(file) for file in final_snapshot.files
-        ),
-    )
+    return BuiltPackage(digest, (BuiltMakeselfBinaryArtifact.create(rendered_path),))
 
 
 def rules():
     return [
         *collect_rules(),
-        UnionRule(PackageFieldSet, MakeselfPackageFieldSet),
+        UnionRule(PackageFieldSet, MakeselfBinaryPackageFieldSet),
     ]
